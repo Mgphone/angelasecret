a2c29d0f97df5e82eec2381b5f6c00aae719652d
Mgphone <51868037+Mgphone@users.noreply.github.com>
2022-12-26 19:47:50 +0000

    use hash encryption

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app=express();
 
 app.use(express.static("public"));
 app.set('view engine','ejs');
-const encrypt=require("mongoose-encryption");
+const md5=require('md5');
 const mongoose=require("mongoose");
 app.use(bodyParser.urlencoded({
   extended:true
@@ -18,9 +18,7 @@ const userSchema=new mongoose.Schema({
   email:String,
   password:String
 });
-const secret=process.env.SECRET;
 
-userSchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
 const User=mongoose.model("User",userSchema);
 app.get("/",function(req,res){
   res.render("home");
@@ -34,7 +32,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
   const newUser= new User({
     email:req.body.username,
-    password:req.body.password
+    password:md5(req.body.password)
   });
   newUser.save(function(err){
     if (err){
@@ -47,7 +45,8 @@ app.post("/register",function(req,res){
 });
 app.post("/login",function(req,res){
 const username=req.body.username;
-const password=req.body.password;
+const password=md5(req.body.password);
+
 User.findOne({email:username},function(err,result){
   if(err){
     console.log(err);
@@ -65,4 +64,5 @@ User.findOne({email:username},function(err,result){
 
 app.listen(3000,function(){
   console.log("Server started on port 3000");
+  
 });
