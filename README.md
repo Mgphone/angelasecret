8f0644184f318262efbfcaf6d47735a1b64ef387
Mgphone <51868037+Mgphone@users.noreply.github.com>
2022-12-26 21:20:34 +0000

    Level 4 add bcrypt

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Authenication...
 Level 1. just simple two ways authenication
 Level 2. create .env files and .gitignore file
 Whenever u want to create need to think the env first with gitignore.
+level 4..bcrypt using.. password +salt... in salt you can make as much as you like salt round. the more salt round up the more difficult to encryt as weel as the more your server need to get more runtime
--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const app=express();
 
 app.use(express.static("public"));
 app.set('view engine','ejs');
-const md5=require('md5');
+const bcrypt=require("bcrypt");
+const saltRounds=10;
 const mongoose=require("mongoose");
 app.use(bodyParser.urlencoded({
   extended:true
@@ -30,22 +31,27 @@ app.get("/register",function(req,res){
   res.render("register");
 });
 app.post("/register",function(req,res){
-  const newUser= new User({
-    email:req.body.username,
-    password:md5(req.body.password)
-  });
-  newUser.save(function(err){
-    if (err){
-      console.log(err);
-    }
-    else{
-      res.render("secrets");
-    }
-  });
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    // Store hash in your password DB.
+    const newUser= new User({
+      email:req.body.username,
+      password:hash
+    });
+    newUser.save(function(err){
+      if (err){
+        console.log(err);
+      }
+      else{
+        res.render("secrets");
+      }
+    });
+});
+  
 });
 app.post("/login",function(req,res){
 const username=req.body.username;
-const password=md5(req.body.password);
+const password=req.body.password;
 
 User.findOne({email:username},function(err,result){
   if(err){
@@ -53,12 +59,17 @@ User.findOne({email:username},function(err,result){
   }
   if(!err){
     if(result){
-      if(result.password===password){
-        res.render("secrets");
+      bcrypt.compare(password, result.password, function(err, result) {
+        // result == true
+        if (result===true){
+          res.render("secrets");
+        }
+    });
+       
       }
     }
-  }
-})
+  })
+
 });
 
 
