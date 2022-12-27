2633ac13d7383eb9e962c3e9313ea794a207893b
Mgphone <51868037+Mgphone@users.noreply.github.com>
2022-12-27 13:54:40 +0000

    main

--- README.md
+++ README.md
@@ -3,3 +3,14 @@ Level 1. just simple two ways authenication
 Level 2. create .env files and .gitignore file
 Whenever u want to create need to think the env first with gitignore.
 level 4..bcrypt using.. password +salt... in salt you can make as much as you like salt round. the more salt round up the more difficult to encryt as weel as the more your server need to get more runtime
+recap the process for level 5.
+
+1.first create 3 requirement modules
+
+1.1set session 
+
+2.passport intialize and passport session
+
+3.plugin passport-local-mongoose
+
+4.passport create stratgey.. serialize..deseriaize(local login setup)
--- app.js
+++ app.js
@@ -3,75 +3,103 @@ require('dotenv').config()
 const express=require("express");
 const ejs= require("ejs")
 const bodyParser=require("body-parser");
+const session=require("express-session");
+const passport=require("passport");
+const passportLocalMongoose=require("passport-local-mongoose");
 const app=express();
-
-app.use(express.static("public"));
-app.set('view engine','ejs');
-const bcrypt=require("bcrypt");
-const saltRounds=10;
 const mongoose=require("mongoose");
+const e = require('express');
 app.use(bodyParser.urlencoded({
   extended:true
 }));
+//session usage
+app.use(session({
+  secret:"Our little Secret",
+  resave: false,
+  saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.static("public"));
+app.set('view engine','ejs');
+
+
+
+
 mongoose.set('strictQuery',false);
 mongoose.connect("mongodb+srv://mgphone:mgphone@cluster0.ccftyls.mongodb.net/userDB");
 const userSchema=new mongoose.Schema({
   email:String,
   password:String
 });
+userSchema.plugin(passportLocalMongoose);
 
 const User=mongoose.model("User",userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 app.get("/",function(req,res){
   res.render("home");
 });
 app.get("/login",function(req,res){
   res.render("login");
 });
+
 app.get("/register",function(req,res){
   res.render("register");
 });
+app.get("/secrets",function(req,res){
+  if (req.isAuthenticated()){
+    res.render("secrets");
+  }
+  else{
+    res.redirect("/login");
+  }
+
+});
 app.post("/register",function(req,res){
-
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    // Store hash in your password DB.
-    const newUser= new User({
-      email:req.body.username,
-      password:hash
-    });
-    newUser.save(function(err){
-      if (err){
-        console.log(err);
-      }
-      else{
-        res.render("secrets");
-      }
-    });
-});
-  
-});
-app.post("/login",function(req,res){
-const username=req.body.username;
-const password=req.body.password;
-
-User.findOne({email:username},function(err,result){
+User.register({username:req.body.username},req.body.password,function(err,user){
   if(err){
     console.log(err);
+    res.redirect("/register")
   }
-  if(!err){
-    if(result){
-      bcrypt.compare(password, result.password, function(err, result) {
-        // result == true
-        if (result===true){
-          res.render("secrets");
-        }
+  else{
+    passport.authenticate("local")(req,res,function(){
+      res.redirect("/secrets")
     });
-       
-      }
-    }
-  })
+  }
+});
+ 
+});
+  
+
+app.post("/login",function(req,res){
+const user=new User({
+  username:req.body.username,
+  password:req.body.password
+});
+req.logIn(user,function(err){
+  if (err){
+    console.log(err);
+  }else{
+    passport.authenticate("local")(req,res,function(){
+      res.redirect("secrets");
+    });
+  }
+})
 
 });
-
+app.get("/logout",function(req,res,next){
+  req.logout(function(err){
+    if (err){
+      return next(err);
+    }
+    res.redirect("/");
+  });
+  
+});
 
 app.listen(3000,function(){
   console.log("Server started on port 3000");
